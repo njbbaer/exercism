5a1106a41012ed70516dda94fbbc4782ee33bf5f
Nathaniel Baer <njbbaer@gmail.com>
2017-09-24 21:35:14 -0700

    Optimize nth-prime

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -3,7 +3,9 @@ module Prime
     raise ArgumentError if n < 1
     primes = []
     2.step do |i|
+      sqrt_i = Math.sqrt(i)
       primes.push i if primes.none? do |p|
+        break true if p > sqrt_i
         (i % p).zero?
       end
       return i if primes.size == n
